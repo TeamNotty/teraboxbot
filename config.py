2f70361beca5b00af2f17e6968bbc50aca88afc0
TeamNotty <nottyboy2026@gmail.com>
2026-02-05 14:37:44 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,9 @@
+API_ID = 123456
+API_HASH = "your_api_hash"
+BOT_TOKEN = "your_bot_token"
+
+# xAPIverse Key
+TERABOX_KEY = "sk_b1007af7ec00ac92596eba542be633c5"
+
+# Admin ID (broadcast ke liye)
+ADMIN_ID = 123456789
